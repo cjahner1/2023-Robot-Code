2c3cb120d2f4d2f224d0f40321527d07ed5a3159
Typeist <106257307+cjahner1@users.noreply.github.com>
2023-01-28 12:09:41 -0800

    drive dummy functions
    dummy functions for drive to build off of for alignment code

--- src/main/java/frc/robot/subsystems/DriveSubsystem.java
+++ src/main/java/frc/robot/subsystems/DriveSubsystem.java
@@ -4,12 +4,14 @@
 
 package frc.robot.subsystems;
 
+import edu.wpi.first.math.geometry.Translation2d;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
 
 public class DriveSubsystem extends SubsystemBase {
   /** Creates a new DriveSubsystem. */
   public DriveSubsystem() {}
-
+  public void drive(Translation2d translation, double rotation, boolean fieldRelative, boolean isOpenLoop) {}
+  public void rotate(double aaa) {}
   @Override
   public void periodic() {
     // This method will be called once per scheduler run
